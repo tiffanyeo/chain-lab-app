43172251b145708075dea6cbe9484f78902feaf4
Tiffany <tiffanylarsson.stud@gmail.com>
2026-04-30 11:58:37 +0100

    chore: add hash function

--- api/blockchain/Hash.ts
+++ api/blockchain/Hash.ts
@@ -1,1 +1,15 @@
 // SHA-256
+
+
+export async function SHA256(data: string): Promise<string> {
+    
+    // Convert string to bytes (SHA-256 requires bytes)
+    const bytes = new TextEncoder().encode(data);
+    
+    // Run SHA, returns hashed bytes
+    const hashBytes = await crypto.subtle.digest("SHA-256", bytes);
+    const numbers = new Uint8Array(hashBytes);
+    return Array.from(numbers)
+        .map(currByte => currByte.toString(16))
+        .join("");
+}

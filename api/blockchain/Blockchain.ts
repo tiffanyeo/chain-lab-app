50544919f363c93967006afdd2df62dde846d059
Tiffany <tiffanylarsson.stud@gmail.com>
2026-05-03 10:38:44 +0100

    feat: implement block header hashing

--- api/blockchain/Blockchain.ts
+++ api/blockchain/Blockchain.ts
@@ -1,1 +1,53 @@
-// BLOCK + CHAIIN
+// BLOCK + CHAIN
+
+import { SHA256 } from "./Hash";
+
+export interface Transaction {
+    id: string,
+    from: string,
+    to: string,
+    amount: number
+}
+
+export interface Block {
+    index: number,
+    timestamp: string,
+    data: { transactions: Transaction[] },
+    previousHash: string,
+    hash: string,
+    nonce: number
+}
+
+export async function createBlock(
+    index: number,
+    data: Block["data"],
+    previousHash: string
+): Promise<Block> {
+
+    // Header components
+    let nonce = 0
+    let hash = ""
+    const timestamp = Date.now()
+    const txString = data.transactions
+        .map(tx => `${tx.id}${tx.from}${tx.to}${tx.amount}`)
+        .join("")
+    
+    // PoW-loop 
+    while (!hash.startsWith("0")) {
+        nonce++;
+        const blockHeaderHash = `${index}${timestamp}${txString}${previousHash}${nonce}`
+        hash = await SHA256(blockHeaderHash)
+    }
+
+    // Created block
+    const block = {
+        index,
+        timestamp: String(timestamp),
+        data,
+        previousHash,
+        hash,
+        nonce
+    }
+    return block
+    
+}
--- api/blockchain/Hash.ts
+++ api/blockchain/Hash.ts
@@ -1,15 +1,19 @@
+import { Block } from "./Blockchain";
+
 // SHA-256
-
-
 export async function SHA256(data: string): Promise<string> {
-    
+
     // Convert string to bytes (SHA-256 requires bytes)
     const bytes = new TextEncoder().encode(data);
-    
-    // Run SHA, returns hashed bytes
+
+    // Run SHA, returns hashed bytes, then convert hash to nums
     const hashBytes = await crypto.subtle.digest("SHA-256", bytes);
     const numbers = new Uint8Array(hashBytes);
-    return Array.from(numbers)
+
+    // Turn nums to HEX and join to a string
+    const joinedString = Array.from(numbers)
         .map(currByte => currByte.toString(16))
         .join("");
+
+    return joinedString;
 }
